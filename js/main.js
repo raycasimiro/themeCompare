cadc271d3260f14b29223ad819f0ef654da88f9a
raycasimiro <54532910+raycasimiro@users.noreply.github.com>
2021-10-06 22:00:02 +0800

    simple code cleanup (main.js)

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ const btnReset = document.getElementById('btn_reset');
 const themeA = document.getElementById('theme_A');
 const themeB = document.getElementById('theme_B');
 const iconDisplay = document.getElementById('icon_display');
-let themeA_obj = {};
-let themeB_obj = {};
 
 themeA.addEventListener('keyup', ()=>prettyPrint(themeA));
 themeA.addEventListener('focus', function(){this.select();});
-themeA.addEventListener('change', function(){iconDisplay.src = "./assets/undef.svg"; themeA_obj = {}});
+themeA.addEventListener('input', function(){
+    iconDisplay.src = "./assets/undef.svg"; 
+});
 themeB.addEventListener('keyup', ()=>prettyPrint(themeB));
 themeB.addEventListener('focus', function(){this.select();});
-themeB.addEventListener('change', function(){iconDisplay.src = "./assets/undef.svg"; themeB_obj = {}});
-
+themeB.addEventListener('input', function(){
+    iconDisplay.src = "./assets/undef.svg"; 
+});
 btnReset.addEventListener('click', function(){
-    themeA_obj = {};
-    themeB_obj = {};
     iconDisplay.src = "./assets/undef.svg";
 });
 
@@ -32,51 +31,35 @@ function prettyPrint(e){
     e.value = pretty
 }
 
+function convertJSONtoObj(jsonString){
+    const obj = JSON.parse(jsonString);
+    const newObj = {};
+
+    Object.keys(obj).forEach((key) => {
+        if(key === 'paneldefaults'){
+            obj[key].forEach((child) => {
+                newObj[child.match_panel_title] = { 
+                    options: child.options, 
+                    styles: child.styles, 
+                    contents: child.contents, 
+                    usable: child.usable 
+                };
+            })
+        }
+        else if(key === 'themeStyle'){
+            newObj['Metadata'] = { ...obj[key].metadata};
+            newObj['Styles'] = { ...obj[key].styles};
+        }
+    });
+    return newObj;
+}
+
 btnParseJSON.addEventListener('click', function(e){
+
     e.preventDefault();
-    // const obj_A = themeA.value !== '' ? JSON.parse(themeA.value) : JSON.parse('{}');
-    // const obj_B = themeB.value !== '' ? JSON.parse(themeB.value) : JSON.parse('{}');
 
-    const obj_A = JSON.parse(themeA.value);
-    const obj_B = JSON.parse(themeB.value);
-
-    Object.keys(obj_A).forEach((key) => {
-        if( key === 'paneldefaults' ){
-            obj_A[key].forEach((item) => {
-                themeA_obj[item.match_panel_title] = { 
-                    options: item.options, 
-                    styles: item.styles, 
-                    contents: item.contents, 
-                    usable: item.usable 
-                };
-            })
-        }
-        else if( key === 'themeStyle' ){
-            themeA_obj['Metadata'] = { ...obj_A[key].metadata};
-            themeA_obj['Styles'] = { ...obj_A[key].styles};
-        }
-    });
-
-    Object.keys(obj_B).forEach((key) => {
-        if( key === 'paneldefaults' ){
-            obj_B[key].forEach((item) => {
-                themeB_obj[item.match_panel_title] = { 
-                    options: item.options, 
-                    styles: item.styles, 
-                    contents: item.contents, 
-                    usable: item.usable 
-                };
-            })
-        }
-        else if( key === 'themeStyle' ){
-            themeB_obj['Metadata'] = { ...obj_B[key].metadata};
-            themeB_obj['Styles'] = { ...obj_B[key].styles};
-        }
-    });
-
-
-    const themeA_JSON = JSON.stringify(themeA_obj);
-    const themeB_JSON = JSON.stringify(themeB_obj);
+    const themeA_JSON = JSON.stringify(convertJSONtoObj(themeA.value));
+    const themeB_JSON = JSON.stringify(convertJSONtoObj(themeB.value));
 
     if(themeA_JSON!=='{}' && themeB_JSON!=='{}'){
 
